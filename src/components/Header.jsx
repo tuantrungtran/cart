3a60522073d93e40b2784991a070994ebe089216
trung.tt <trung@kvytechnology.com>
2018-05-09 16:56:15 +0700

    deploy

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {findDOMNode} from 'react-dom';
+import React, {Component} from 'react'
+import {findDOMNode} from 'react-dom'
 import EmptyCart from '../empty/EmptyCart'
 import CartScrollBar from './CartScrollBar'
 
